10943dc2c78668cf905e9cba8635b61778fb43ec
Emiel van Lankveld <emielvanlankveld@voormedia.com>
2021-09-09 19:34:32 +0200

    Add logging.

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,7 +1,11 @@
 import { PluginOptions } from './gatsby-ssr'
 
 export function checkPathInclusion (pathname: string, pluginOptions: PluginOptions): boolean {
+  console.log("test")
   if (!pluginOptions.includePaths) return false
-
+  console.log(pluginOptions.includePaths)
+  console.log(pathname)
+  console.log(RegExp(pluginOptions.includePaths).test(pathname))
+  console.log(RegExp(pluginOptions.includePaths))
   return RegExp(pluginOptions.includePaths).test(pathname)
 }
